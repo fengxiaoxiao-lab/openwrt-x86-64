169377259000136cca3b5be5e5e79fc847d06946
fengxiaoxiao-lab <65667743+fengxiaoxiao-lab@users.noreply.github.com>
2020-05-21 21:27:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #Add AdGuardHome source
-sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
-#sed -i '$a src-git adguardhome https://github.com/AdguardTeam/AdGuardHome' feeds.conf.default
+#sed -i '$a src-git adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
+sed -i '$a src-git adguardhome https://github.com/AdguardTeam/AdGuardHome' feeds.conf.default
 #sed -i '$a src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' feeds.conf.default
