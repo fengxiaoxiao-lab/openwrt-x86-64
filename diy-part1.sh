efc96448ff8d1da0948b8d947296ba35d9401ce0
fengxiaoxiao-lab <65667743+fengxiaoxiao-lab@users.noreply.github.com>
2020-05-22 13:52:26 +0800

    修改
    修改

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,9 @@
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git smartdns https://github.com/pymumu/smartdns' feeds.conf.default
+sed -i '$a src-git luci-app-smartdns https://github.com/pymumu/luci-app-smartdns' feeds.conf.default
