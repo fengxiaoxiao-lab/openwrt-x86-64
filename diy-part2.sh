9e2a8a7b2c05eea790afc45235c3af1239bcdffa
fengxiaoxiao-lab <65667743+fengxiaoxiao-lab@users.noreply.github.com>
2020-05-22 16:56:07 +0800

    最终版本
    最终版本

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 # Modify luci-app-smartdns makefile
-sed -i 's#luci.mk#feeds/luci/luci.mk#' openwrt/package/luci-app-smartdns/Makefile
+sed -i 's#luci.mk#feeds/luci/luci.mk#' package/luci-app-smartdns/Makefile
