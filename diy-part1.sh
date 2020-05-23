5bb365b0cae3c29e6e51a38a800725ecb47aee6f
fengxiaoxiao-lab <65667743+fengxiaoxiao-lab@users.noreply.github.com>
2020-05-23 14:10:54 +0800

    修改
    修改

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,4 +14,3 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git smartdns https://github.com/pymumu/smartdns' feeds.conf.default
-sed -i '$a src-git luci-theme-opentomcat https://github.com/Leo-Jo-My/luci-theme-opentomcat' feeds.conf.default
